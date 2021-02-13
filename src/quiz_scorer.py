4ab37ff4c2fcb156cc372d321695048b81a461a6
Ignacio Paricio <ignacio.paricio@outlook.com>
2021-02-13 19:54:05 +0100

    Add support for google_forms outputs

--- src/quiz_scorer.py
+++ src/quiz_scorer.py
@@ -16,9 +16,7 @@ class QuizScorer:
         """Instantiates a QuizScorer."""
         with open("../conf/config.yml") as file:
             config = yaml.full_load(file)
-        self.correct_answers = pd.read_csv(
-            "../scoring/correct_answers.csv", index_col="Name"
-        )
+        self.read_from = config.get("read_from")
         self.answers_dir = "../scoring/answers"
         self.answers = None
         self.outputs_dir = "../outputs"
@@ -31,12 +29,17 @@ class QuizScorer:
         self.bands = config.get("bands")
         self.x_offset_plot = 0.1
         self.plot_title = "Game scores"
+        if self.read_from == "manual_csvs":
+            self.correct_answers = pd.read_csv(
+                "../scoring/answers/correct_answers.csv", index_col="Name"
+            )
+        if self.read_from == "google_forms":
+            self.name_col = config.get("name_col")
 
     def generate_scores(self):
         """Runs end-to-end scores processing."""
         self._fetch_answers()
         self._process_answers()
-        self._save_ncorrect_matrix()
         self._build_scores_plot()
 
     def generate_mock_plot(self):
@@ -67,22 +70,50 @@ class QuizScorer:
         answers = {}
         owd = os.getcwd()
         os.chdir(self.answers_dir)
+        load_args = {"index_col": "Name"} if self.read_from == "manual_csvs" else {}
         for file in os.listdir():
             name = os.path.splitext(file)[0]  # Strip file extension
-            answers[name] = pd.read_csv(file, index_col="Name")
+            answers[name] = pd.read_csv(file, **load_args)
         os.chdir(owd)
         self.answers = answers
 
     def _process_answers(self):
         """Scores each answer against the template from `scoring/correct_answers.csv"""
-        self.ncorrect_matrix = pd.DataFrame(
-            index=self.correct_answers.index, columns=self.correct_answers.columns
-        ).fillna(0)
-        for name, answer in self.answers.items():
-            guessed = answer == self.correct_answers
-            self.scores.append(guessed.sum().sum())
-            self.names.append(name)
-            self.ncorrect_matrix = self.ncorrect_matrix.add(guessed)
+        if self.read_from == "manual_csvs":
+            self.ncorrect_matrix = pd.DataFrame(
+                index=self.correct_answers.index, columns=self.correct_answers.columns
+            ).fillna(0)
+            for name, answer in self.answers.items():
+                guessed = answer == self.correct_answers
+                self.scores.append(guessed.sum().sum())
+                self.names.append(name)
+                self.ncorrect_matrix = self.ncorrect_matrix.add(guessed)
+            self._save_ncorrect_matrix()
+
+        if self.read_from == "google_forms":
+            answers_df = self.answers.copy().popitem()[1]  # Only one csv expected
+            answers_df = answers_df.set_index(self.name_col)
+
+            def is_float(string):
+                try:
+                    float(string)
+                    return True
+                except ValueError:
+                    return False
+
+            def score_parser(str_with_nums):
+                return [float(s) for s in str_with_nums.split() if is_float(s)][0]
+
+            answers_df = answers_df[
+                [
+                    col
+                    for col in answers_df.columns
+                    if "[Score]" in col and self.name_col not in col
+                ]
+            ].applymap(score_parser)
+            answers_df["Score"] = answers_df.sum(axis=1)
+            self.names = answers_df.index.tolist()
+            self.scores = answers_df["Score"].tolist()
 
     def _save_ncorrect_matrix(self):
         """Saves a matrix with the number of correct guesses per field and category."""
